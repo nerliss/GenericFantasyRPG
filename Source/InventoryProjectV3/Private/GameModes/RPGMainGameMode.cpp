4937afaef6288afda66e86bb0253aab37aa42b9b
nerliss <nerliss228@gmail.com>
2022-05-16 17:30:40 +0300

    C++ gamemode base

---
+++ Source/InventoryProjectV3/Private/GameModes/RPGMainGameMode.cpp
@@ -0,0 +1,9 @@
+// Destruction Games. 2022
+
+
+#include "GameModes/RPGMainGameMode.h"
+
+ARPGMainGameMode::ARPGMainGameMode()
+{
+
+}
---
+++ Source/InventoryProjectV3/Public/GameModes/RPGMainGameMode.h
@@ -0,0 +1,17 @@
+// Destruction Games. 2022
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPGMainGameMode.generated.h"
+
+UCLASS()
+class INVENTORYPROJECTV3_API ARPGMainGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+
+	ARPGMainGameMode();
+};
